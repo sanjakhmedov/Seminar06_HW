a03c24276ec028cd11efb3c07d7e9885b4eb62b2
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-28 18:36:43 +0500

    task 43 is done

---
+++ Task43/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются 
+// пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.WriteLine("Enter the values for 4 numbers: ");
+Console.Write("b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+double[] FindCrossPoint(double b1, double k1, double b2, double k2)
+{
+    double[] res = new double[2];
+    res[0] = (b2 - b1) / (k1 - k2);
+    res[1] = k2 * res[0] + b2;
+    return res;
+}
+
+double[] result = FindCrossPoint(b1, k1, b2, k2);
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, "); // $"{Math.Round(array[i], 4)}, "
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.WriteLine("]");
+}
+
+PrintArray(result);
